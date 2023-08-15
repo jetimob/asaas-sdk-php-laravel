ba4df06835dcc4862be1919f78ef6ca71456a49a
iamgabrieloliveira <oliveiragabriel.dev@gmail.com>
2023-08-15 17:42:20 -0300

    feat: Adiciona opção de passar o access_token para consultas sobre outras contas

--- src/Api/AbstractApi.php
+++ src/Api/AbstractApi.php
@@ -8,16 +8,24 @@ use Jetimob\Http\Contracts\HttpProviderContract;
 abstract class AbstractApi extends \Jetimob\Http\AbstractApi
 {
     protected ?string $exceptionClass = AsaasRequestException::class;
+    protected string $accessToken;
 
     public function __construct(HttpProviderContract $httpProvider)
     {
         parent::__construct($httpProvider);
+        $this->accessToken = config('asaas.http.access_token');
+    }
+
+    public function usingToken(string $token): self
+    {
+        $this->accessToken = $token;
+        return $this;
     }
 
     protected function makeBaseRequest($method, $path, array $headers = [], $body = null): AuthorizedRequest
     {
         if (!isset($headers['access_token'])) {
-            $headers['access_token'] = config('asaas.http.access_token');
+            $headers['access_token'] = $this->accessToken;
         }
 
         return new AuthorizedRequest($method, $path, $headers, $body);
