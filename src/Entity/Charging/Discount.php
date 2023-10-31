706b3967b72d2c88b07a31f3b05134327ed0f9ac
iamgabrieloliveira <oliveiragabriel.dev@gmail.com>
2023-10-31 18:45:02 -0300

    fix: Corrige criação da entidade de desconto

--- src/Entity/Charging/Discount.php
+++ src/Entity/Charging/Discount.php
@@ -61,6 +61,6 @@ class Discount extends Entity
 
     public static function new(float $value, string $type, int $dueDateLimitDays): static
     {
-        return with(new static())->setValue($value)->setDueDateLimitDays($dueDateLimitDays);
+        return with(new static())->setValue($value)->setType($type)->setDueDateLimitDays($dueDateLimitDays);
     }
 }
