1c1a553f9cfa0624cf185bd999352e2dfb9a4682
iamgabrieloliveira <oliveiragabriel.dev@gmail.com>
2023-11-01 11:37:29 -0300

    feat: Adiciona campo de tipo na entidade de multa

--- src/Entity/Charging/Fine.php
+++ src/Entity/Charging/Fine.php
@@ -7,4 +7,21 @@ use Jetimob\Asaas\Entity\Valuable;
 /** Percentual de multa sobre o valor da cobrança para pagamento após o vencimento */
 class Fine extends Valuable
 {
+    protected ?string $type;
+
+    public function getType(): ValueType
+    {
+        return ValueType::from($this->type);
+    }
+
+    public function setType(?ValueType $type): self
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+    public static function new(float $value, ValueType $type): Fine
+    {
+        return self::withValue($value)->setType($type);
+    }
 }
---
+++ src/Entity/Charging/ValueType.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Jetimob\Asaas\Entity\Charging;
+
+enum ValueType: string
+{
+    case FIXED = 'FIXED';
+    case PERCENTAGE = 'PERCENTAGE';
+}
